0e54511726a2fc74a67a2b7a1614269fae2d90f9
Maggiekocon <sunflowerartist4@gmail.com>
2026-04-06 15:36:41 -0500

    cleaned up branch

--- includes/db.php
+++ includes/db.php
@@ -1,32 +1,12 @@
 <?php
+$host = "localhost";
+$user = "root";
+$pass = 'Passw0rd';
+$db = "booktrackerdb";
 
-// Database configuration
-$db_host = 'localhost';       // Database host
-$db_user = 'root';   // Database username
-$db_pass = 'Passw0rd';   // Database password
-$db_name = 'booktrackerdb';   // Database name
+$conn = new mysqli($host, $user, $pass, $db);
 
-// Create a new MySQLi connection
-$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
-
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-// Optional: set charset to UTF-8
-$conn->set_charset("utf8");
-
-//postgres
-// postgresql://postgres:[YOUR-PASSWORD]@db.lmyatqnydphsmhgbyosn.supabase.co:5432/postgres
-// $host = db.lmyatqnydphsmhgbyosn.supabase.co
-
-// $port = 5432;
-
-// $database = postgres;
-
-// $user =postgres;
-
-
-
 ?>
